15054313be935b05ea950e71e19bfc494ad1b09f
Oski Bear <oski@bear>
2015-11-08 12:12:34 -0800

    no need

---
+++ code/utils/basic_modeling.py
@@ -0,0 +1,47 @@
+import numpy as np
+import numpy.linalg as npl
+import matplotlib.pyplot as plt
+import nibabel as nib
+
+def load_data(f1, f2):
+    """
+    Return the image data as an array and the convolved time course
+
+    Parameters
+    ----------
+    f1,f2 : string
+        The name of data to process
+    Returns
+    -------
+    tuple:  
+        Contains the image data as an array and the convolved time course
+    """
+    # Load the image as an image object
+    img = nib.load('../../data/sub001/BOLD/' + f1 + '.nii.gz')
+    # Load the image data as an array
+    # Drop the first 4 3D volumes from the array
+    data = img.get_data()[..., 4:]
+    # Load the pre-written convolved time course
+    convolved = np.loadtxt('../../data/conv/' + f2 + '.txt')[4:]
+    return(data, convolved)
+
+def reg_voxels_4d(data, convolved):
+    design = np.ones((len(convolved), 2))
+    design[:, 1] = convolved
+    data_2d = np.reshape(data, (-1, data.shape[-1]))
+    betas = npl.pinv(design).dot(data_2d.T)
+    betas_4d = np.reshape(betas.T, img.shape[:-1] + (-1,))
+
+    return betas_4d
+
+if __name__ == '__main__':
+    from sys import argv
+    f1 = argv[1]
+    f2 = argv[2]
+    get_name = f2.replace('/', '_')
+    data, convolved = load_data(f1, f2)
+    beta_hat = reg_voxels_4d(data, convolved)
+    np.savetxt('../../data/beta/' + get_name + '.txt', beta_hat)
+    plt.imshow(beta_hat[:, :, 14, 0], interpolation = 'nearest', cmap = 'gray')
+    plt.show()
+
