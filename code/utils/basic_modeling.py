4977946b7b0ddea6f756d3a8a8784d5c652f4d32
Oski Bear <oski@bear>
2015-11-10 22:59:26 -0800

    upating on 23pm

--- code/utils/basic_modeling.py
+++ code/utils/basic_modeling.py
@@ -1,3 +1,4 @@
+from __future__ import division
 import numpy as np
 import numpy.linalg as npl
 import matplotlib.pyplot as plt
@@ -32,29 +33,35 @@ def reg_voxels_4d(data, convolved):
     data_2d = np.reshape(data, (data.shape[-1], -1))
     betas = npl.pinv(design).dot(data_2d) #(2, 147456)
     betas_4d = np.reshape(betas.T, data.shape[:-1] + (-1,)) #(64, 64, 36, 2)
-    print(design.shape, data_2d.shape, betas.shape)
+    
     return (design, data_2d, betas, betas_4d)   
 
 def RSE(X,Y, betas_hat): #input 2D of X, Y and betas_hat
     Y_hat = X.dot(betas_hat)
     res = Y - Y_hat
     RSS = np.sum(res ** 2, 0)
-    print(RSS.shape)
     df = X.shape[0] - npl.matrix_rank(X)
     MRSS = RSS / df
 
     return (MRSS, df) 
 
-def hypothesis(betas_hat, v_cov, df):
+def hypothesis(betas_hat, beta_cov, df):
+    sd_beta = np.array([])
     t_stat = np.array([])
-    for i in v_cov:
-        t_stat = np.append(t_stat, betas_hat[:, 1] / np.sqrt(i[1,1]))
+    
+    for i in beta_cov:
+        sd_beta = np.append(sd_beta, np.sqrt(i[1,1]))
+
+    print(sd_beta.shape, beta_cov[1,:].shape)
 
     # Get p value for t value suing cumulative density dunction
-    p = np.array([])
-    for j in t_stat:
-        p = np.append(p, 1 - t_dist.cdf(j, df))
-
+    t_stat = beta_cov[1,:] / sd_beta
+    print(t_stat.shape)
+    
+    ltp = t_dist.cdf(t_stat, df) # lower tail p
+    p = 1 - ltp # upper tail p
+    
+    print(p.shape)
     return p
 
 if __name__ == '__main__':
@@ -67,16 +74,16 @@ if __name__ == '__main__':
     np.savetxt('../../data/beta/' + get_name + '.txt', betas_hat, newline='\r\n')
     plt.imshow(betas_hat_4d[:, :, 15, 1])
     plt.savefig('../../data/beta/task001_run001_conv005.png')
-    #plt.show()
+    
     #get residual standard error
     s2, df = RSE(design_mat, data_2d, betas_hat)
-    
+   
     #get estimator of variance of betas_hat   
     beta_cov = np.array([])
     for i in s2:
         beta_cov = np.append(beta_cov, npl.inv(design_mat.T.dot(design_mat)))
     beta_cov = beta_cov.reshape(-1,2,2)
-
+   
     #T-test on null hypothesis
     p_value = hypothesis(betas_hat, beta_cov, df)
     print(p_value)
