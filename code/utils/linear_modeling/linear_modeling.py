c2f134b98a43d7486100cfb7c5a990dbeec2892a
Jay4869 <jli4869@berkeley.edu>
2015-11-30 10:36:28 -0800

    update

---
+++ code/utils/linear_modeling/linear_modeling.py
@@ -0,0 +1,124 @@
+from __future__ import division
+import numpy as np
+import numpy.linalg as npl
+import matplotlib.pyplot as plt
+import nibabel as nib
+from scipy.stats import t as t_dist
+
+""" Linear_modeling.py
+
+    We are going to model signal by incorporating five conditions all convolved
+with hrf, along with a few linear drift terms.
+
+    This is OLS estimation; we assume the errors to have independent and
+identical normal distributions around zero for each i in e_i (i.i.d).
+"""
+
+
+def beta_est(y, X):
+    """
+    parameters
+    ----------
+    y: 2D array (n_vols x n_trs)
+        BOLD data.
+    X: 2D array (n_trs * number of regressors)
+        design matrix.
+
+    Returns
+    ______
+    betas: 2D array (number of regressors x n_vols)
+        estimated betas for linear model.
+    MRSS: 1D array of length n_volx
+        Mean residual sum of squares.
+    df: int
+        n - rank of X.
+    """
+
+    # Make sure y, X, c are all arrays
+    y = np.asarray(y)
+    X = np.asarray(X)
+    # Calculate the parameters - b hat
+    beta = npl.pinv(X).dot(y)
+    # The fitted values - y hat
+    fitted = X.dot(beta)
+    # Residual error
+    errors = y - fitted
+    # Residual sum of squares
+    RSS = (errors**2).sum(axis=0)
+    # Degrees of freedom is the number of observations n minus the number
+    # of independent regressors we have used.  If all the regressor
+    # columns in X are independent then the (matrix rank of X) == p
+    # (where p the number of columns in X). If there is one column that
+    # can be expressed as a linear sum of the other columns then
+    # (matrix rank of X) will be p - 1 - and so on.
+    df = X.shape[0] - npl.matrix_rank(X)
+    # Mean residual sum of squares
+    MRSS = RSS / df
+
+    return beta, MRSS, df
+
+
+def t_stat(X, c, beta, MRSS, df):
+    """
+    parameters
+    ----------
+    X: 2D array (n_trs * number of regressors)
+        design matrix.
+    betas: 2D array (number of regressors x n_vols)
+        estimated betas for linear model.
+    MRSS: 1D array of length n_volx
+        Mean residual sum of squares.
+    df: int
+        n - rank of X.
+
+    Returns
+    ______
+    t statistics: a vector of length n_vols
+        t statistics for each voxel.
+    p values: a vector of length n_vols
+        p values for each voxel.
+    """
+    X = np.asarray(X)
+    c = np.atleast_2d(c).T
+    # calculate bottom half of t statistic
+    SE = np.sqrt(MRSS * c.T.dot(npl.pinv(X.T.dot(X)).dot(c)))
+    t = c.T.dot(beta) / SE
+    # Get p value for t value using cumulative density dunction
+    # (CDF) of t distribution
+    ltp = t_dist.cdf(t, df) # lower tail p
+    p = 1 - ltp # upper tail p
+
+    return t, p
+
+
+def reshape(mask, vol_shape, arr):
+    """
+    We can use 3D mask to index into 4D dataset.
+    This selects all the voxel time-courses for voxels within the brain
+    (as defined by the mask).
+
+    Parameters
+    ----------
+    mask: 3D array
+        The resulting mask of using mean volumes over time.
+    vol_shape: tuple
+        3D shape of voxels.
+    arr: 2D array (int x n_vols)
+        array to be reshaped.
+    Returns
+    -------
+    vol_reshape
+    masked voxel time-course
+
+    Intermediate step example for demonstrate:
+
+    To reshape beta:
+    b_vols = np.zeros(vol_shape + (beta.shape[0],))
+    b_vols[in_brain_mask, :] = beta.T
+    """
+    vols_reshape = np.zeros(vol_shape + (arr.shape[0],))
+    vols_reshape[mask, :] = arr.T
+
+    return vols_reshape
+
+
---
+++ code/utils/linear_modeling/linear_modeling_script.py
@@ -0,0 +1,132 @@
+from __future__ import division
+import numpy as np
+import numpy.linalg as npl
+import matplotlib.pyplot as plt
+import nibabel as nib
+from sys import argv
+import linear_modeling
+
+"""
+set the directory to project-iota and run below command on terminal:
+
+python linear_modeling_script sub001/BOLD/task001_run001/filtered_func_data_mni
+
+"""
+
+######### set gray colormap and nearest neighbor interpolation by default
+#plt.rcParams['image.cmap'] = 'gray'
+#plt.rcParams['image.interpolation'] = 'nearest'
+
+######### Load f1, the BOLD image.
+f1 = argv[1] # sub001/BOLD/task001_run001/bold
+# or use the filtered data: sub001/BOLD/task001_run001/filtered_func_data_mni
+
+#img = nib.load("../../../data/sub001/BOLD/task001_run001/filtered_func_data_mni.nii.gz")
+img = nib.load('../../../data/' + f1 + '.nii.gz')
+data = img.get_data()
+
+######### Get n_trs and voxel shape
+n_trs = data.shape[-1]
+vol_shape = data.shape[:-1]
+
+######### Construct design matrix:
+# Load the convolution files.
+design_mat = np.ones((n_trs, 9))
+files = ('1', '2', '3', '4', '5', '6')
+for file in files:
+    design_mat[:, (int(file) - 1)] = np.loadtxt('../../../data/convo_prep/task001_run001_cond00'
+                                          + file + '_conv.txt')
+# adding the linear drifter terms to the design matrix
+linear_drift = np.linspace(-1, 1, n_trs)
+design_mat[:, 6] = linear_drift
+quadratic_drift = linear_drift ** 2
+quadratic_drift -= np.mean(quadratic_drift)
+design_mat[:, 7] = quadratic_drift
+# show the design matrix graphically:
+plt.imshow(design_mat, aspect=0.1)
+#plt.show()
+#plt.savefig('../../../data/design_matrix/design_mat.png')
+
+
+######### we take the mean volume (over time), and do a histogram of the values
+mean_vol = np.mean(data, axis=-1)
+plt.hist(np.ravel(mean_vol), bins=100)
+plt.xlabel('Voxels')
+plt.ylabel('Frequency')
+plt.title('Mean Volume Over Time')
+#plt.show()
+#plt.savefig("../../../data/design_matrix/mean_vol.png")
+# mask out the outer-brain noise using mean volumes over time.
+in_brain_mask = mean_vol > 5000
+# We can use this 3D mask to index into our 4D dataset.
+# This selects all the voxel time-courses for voxels within the brain
+# (as defined by the mask)
+in_brain_tcs = data[in_brain_mask, :]
+
+
+
+######### Lastly, do t test on betas:
+y = in_brain_tcs.T
+X = design_mat
+
+beta, MRSS, df = linear_modeling.beta_est(y,X)
+
+# Visualizing betas for the middle slice
+# First reshape
+b_vols = linear_modeling.reshape(in_brain_mask, vol_shape, beta)
+# Then plot them on the same plot with uniform scale
+fig, axes = plt.subplots(nrows=2, ncols=4)
+for i, ax in zip(range(0,8,1), axes.flat):
+    im = ax.imshow(b_vols[:, :, 45, i], cmap = 'jet')
+fig.subplots_adjust(right=0.85)
+cax = fig.add_axes([0.9, 0.15, 0.03, 0.7])
+fig.colorbar(im, cax=cax)
+#plt.show()
+
+# To test significance of betas:
+# Create contrast matrix for each beta:
+c_mat = np.diag(np.array(np.ones((9,))))
+# t statistics and p values
+# Length is the number of voxels after masking
+t_mat = np.ones((9, y.shape[1]))
+p_mat = np.zeros((9, y.shape[1],))
+for i in range(0,9,1):
+    t, p = linear_modeling.t_stat(X, c_mat[:,i], beta, MRSS, df)
+    t_mat[i,:] = t
+    p_mat[i,:] = p
+# save the t values and p values in txt files.
+#np.savetxt('../../../data/maps/t_stat.txt', t_mat)
+#np.savetxt('../../../data/maps/p_val.txt', p_mat)
+
+# Reshape t values
+t_val = linear_modeling.reshape(in_brain_mask, vol_shape, t_mat)
+# Reshape p values
+p_val = linear_modeling.reshape(in_brain_mask, vol_shape, p_mat)
+
+# Visualizing t values for the middle slice
+fig, axes = plt.subplots(nrows=2, ncols=4)
+for i, ax in zip(range(0,8,1), axes.flat):
+    im = ax.imshow(t_val[:, :, 45, i], cmap = 'RdYlBu')
+fig.subplots_adjust(right=0.85)
+cax = fig.add_axes([0.9, 0.15, 0.03, 0.7])
+fig.colorbar(im, cax=cax)
+plt.show()
+
+# Visualizing p values for the middle slice in color
+fig, axes = plt.subplots(nrows=2, ncols=4)
+for i, ax in zip(range(0,8,1), axes.flat):
+    im = ax.imshow(p_val[:, :, 45, i], cmap = 'RdYlBu')
+fig.subplots_adjust(right=0.85)
+cax = fig.add_axes([0.9, 0.15, 0.03, 0.7])
+fig.colorbar(im, cax=cax)
+plt.show()
+
+# Visualizing p values for the middle slice in gray
+fig, axes = plt.subplots(nrows=2, ncols=4)
+for i, ax in zip(range(0,8,1), axes.flat):
+    im = ax.imshow(p_val[:, :, 45, i], cmap = 'gray')
+fig.subplots_adjust(right=0.85)
+cax = fig.add_axes([0.9, 0.15, 0.03, 0.7])
+fig.colorbar(im, cax=cax)
+plt.show()
+
